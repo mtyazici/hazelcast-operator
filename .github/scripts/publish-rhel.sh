9f52294d7e33938b59b3002dbcf9bca2bcb15b00
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2020-12-10 10:43:32 +0300

    Fixes

--- .github/scripts/publish-rhel.sh
+++ .github/scripts/publish-rhel.sh
@@ -4,13 +4,13 @@ get_containers()
 {
     PROJECT_ID=$1
     VERSION=$2
-    API_KEY=$3
+    RHEL_API_KEY=$3
 
     RESPONSE=$( \
         curl --silent \
              --request POST \
              --header "Content-Type: application/json" \
-             --header "Authorization: Bearer ${API_KEY}" \
+             --header "Authorization: Bearer ${RHEL_API_KEY}" \
              --data {} \
              "https://connect.redhat.com/api/v2/projects/${PROJECT_ID}/tags?tags=${VERSION}")
 
@@ -21,12 +21,12 @@ get_container_build()
 {
     PROJECT_ID=$1
     VERSION=$2
-    API_KEY=$3
+    RHEL_API_KEY=$3
 
-    BUILD=$(get_containers "${PROJECT_ID}" "${VERSION}" "${API_KEY}" | jq -r '.tags[0]')
+    BUILD=$(get_containers "${PROJECT_ID}" "${VERSION}" "${RHEL_API_KEY}" | jq -r '.tags[0]')
     if [ "${BUILD}" == "null" ]; then
         # TAG can be also stored as "${VERSION}, latest"
-        BUILD=$(get_containers "${PROJECT_ID}" "${VERSION}%2C%20latest" "${API_KEY}" | jq -r '.tags[0]')
+        BUILD=$(get_containers "${PROJECT_ID}" "${VERSION}%2C%20latest" "${RHEL_API_KEY}" | jq -r '.tags[0]')
     fi
     echo "${BUILD}"
 }
@@ -39,55 +39,51 @@ is_build_valid()
     return $?
 }
 
-# Check parameters
-if [ "$#" -ne 3 ]; then
-    echo "Illegal number of parameters"
-    exit 1
-fi
 
-# Assign API Key and Project ID
-API_KEY=$1
-PROJECT_ID=$2
-VERSION=$3
+wait_for_container_build_or_scan()
+{
+    # Wait until the image is built and scanned
+    for i in `seq 1 10`; do
+        BUILD=$(get_container_build "${PROJECT_ID}" "${VERSION}" ${RHEL_API_KEY})
+        SCAN_STATUS=$(echo "${BUILD}" | jq -r '.scan_status')
+        DIGEST=$(echo "${BUILD}" | jq -r '.digest')
+        if [ "${SCAN_STATUS}" == "passed" ]; then
+            break
+        fi
+        echo "Building or scanning in progress, waiting..."
+        sleep 120
+        if [ "$i" = "10" ]; then
 
-# Wait until the image is built and scanned
-for i in `seq 1 10`; do
-    BUILD=$(get_container_build "${PROJECT_ID}" "${VERSION}" ${API_KEY})
-    SCAN_STATUS=$(echo "${BUILD}" | jq -r '.scan_status')
-    DIGEST=$(echo "${BUILD}" | jq -r '.digest')
-    if [ "${SCAN_STATUS}" == "passed" ]; then
-        break
-    fi
-    echo "Building or scanning in progress, waiting..."
-    sleep 120
-    if [ "$i" = "10" ]; then
+        fi
+    done
+}
 
-    fi
-done
+publish_the_image()
+{
+    # Publish the image
+    echo "Publishing the image..."
+    RESPONSE=$( \
+    #    curl --silent \
+            --request POST \
+            --header "Authorization: Bearer ${RHEL_API_KEY}" \
+            --header 'Cache-Control: no-cache' \
+            --header 'Content-Type: application/json' \
+            --data {} \
+            "https://connect.redhat.com/api/v2/projects/${PROJECT_ID}/containers/${DIGEST}/tags/${VERSION}/publish")
+    STATUS=$(echo "${RESPONSE}" | jq -r '.status')
 
-# Publish the image
-echo "Publishing the image..."
-RESPONSE=$( \
-    curl --silent \
-         --request POST \
-         --header "Authorization: Bearer ${API_KEY}" \
-         --header 'Cache-Control: no-cache' \
-         --header 'Content-Type: application/json' \
-         --data {} \
-         "https://connect.redhat.com/api/v2/projects/${PROJECT_ID}/containers/${DIGEST}/tags/${VERSION}/publish")
-STATUS=$(echo "${RESPONSE}" | jq -r '.status')
-
-# Result message
-if [ "${STATUS}" == "OK" ]; then
-    echo "Done."
-    exit 0
-else
-    ERROR=$(echo "${RESPONSE}" | jq -r '.data.errors[0]')
-    if [[ "${ERROR}" == 'Container image is already published'* ]]; then
-        echo "Image is already published. Skipped."
+    # Result message
+    if [ "${STATUS}" == "OK" ]; then
+        echo "Done."
         exit 0
     else
-        echo "Error, result message: ${RESPONSE}"
-        exit 42
+        ERROR=$(echo "${RESPONSE}" | jq -r '.data.errors[0]')
+        if [[ "${ERROR}" == 'Container image is already published'* ]]; then
+            echo "Image is already published. Skipped."
+            exit 0
+        else
+            echo "Error, result message: ${RESPONSE}"
+            exit 42
+        fi
     fi
-fi
+}
