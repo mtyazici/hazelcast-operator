83ec4fcdbf4eb0e09039e947c135c02fcf3e92d7
Muhammet Yazici <muhammet.yazici@boun.edu.tr>
2021-03-04 19:17:04 +0300

    Fix

--- .github/scripts/clean-up.sh
+++ .github/scripts/clean-up.sh
@@ -7,10 +7,16 @@ set -o pipefail
 WORKDIR=$1
 PROJECT=$2
 
-oc delete -f ${WORKDIR}/hazelcast.yaml
-oc delete -f ${WORKDIR}/secret.yaml
-oc delete -f ${WORKDIR}/bundle.yaml
-oc delete secret pull-secret
+oc delete -f --wait=true ${WORKDIR}/hazelcast.yaml 
+oc delete -f --wait=true ${WORKDIR}/secret.yaml 
+oc delete -f --wait=true ${WORKDIR}/hazelcast-rbac.yaml
+
+oc delete --wait=true rolebinding hazelcast-enterprise-operator
+oc delete --wait=true clusterrole hazelcast-enterprise-operator
+oc delete --wait=true serviceaccount hazelcast-enterprise-operator
+oc delete --wait=true deployment hazelcast-enterprise-operator
+
+oc delete --wait=true secret pull-secret
 oc delete project $PROJECT
 
 oc logout
